e8bf69bf2e552de600cf860962df65290c3cc844
simonszc <zsimonson26@gmail.com>
2016-06-02 14:59:59 -0700

    fixed dbport

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 // const errorHandler = require('./lib/error_handling.js');
 
-const dbPort =process.env.MONGOLAB_URI || 'mongodb://localhost/dev_db';
+const dbPort =process.env.MONGODB_URI || 'mongodb://localhost/dev_db';
 
 mongoose.connect(dbPort);
 
